1ef2382db1600b9457477d9ecd2118ace6f8a219
Thomas Tykesson <thomas@tykesson.com>
2024-09-27 08:44:33 +0200

    Initial commit: Next.XP app based on Next.XP tutorial, with some in-progress changes

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+	env: {
+	},
+
+	extends: [
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:jsx-a11y/recommended'
+	],
+
+	ignorePatterns: [
+		"build/**/*.*"
+	],
+
+	overrides: [{
+		files: [
+			'**/*.ts',
+			'**/*.tsx'
+		]
+	}],
+
+	plugins: [
+		'jsx-a11y',
+		'@typescript-eslint'
+	],
+
+	root: true,
+
+	rules: { // https://eslint.org/docs/rules
+		'no-unused-vars': ["warn", { "argsIgnorePattern": "^_" }],
+		'@typescript-eslint/no-unused-vars': ["warn", { "argsIgnorePattern": "^_" }]
+	},
+
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	}
+}; // module.exports
