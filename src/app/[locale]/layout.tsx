284ed318247bb9f0e6e82ce42290ae92ac35c219
Thomas Tykesson <thomas@tykesson.com>
2024-12-17 09:16:52 +0100

    Added Mundial fonts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import {I18n, PORTAL_COMPONENT_ATTRIBUTE} from '@enonic/nextjs-adapter';
 import {Metadata} from 'next';
 import {ReactNode} from 'react';
+import localFont from 'next/font/local'
 
 import '../../styles/globals.css';
 
@@ -11,6 +12,42 @@ type LayoutProps = {
     children: ReactNode
 }
 
+const mundial = localFont({
+    variable: '--font-mundial',
+    src: [
+        {
+            path: '../fonts/3-mundial-light-tty.woff2',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path: '../fonts/3-mundial-light-italic-tty.woff2',
+            weight: '300',
+            style: 'italic',
+        },
+        {
+            path: '../fonts/4-mundial-regular-tty.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../fonts/4-mundial-regular-italic-tty.woff2',
+            weight: '400',
+            style: 'italic',
+        },
+        {
+            path: '../fonts/5-mundial-demibold-tty.woff2',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: '../fonts/5-mundial-demibold-italic-tty.woff2',
+            weight: '500',
+            style: 'italic',
+        },    
+    ],
+})
+
 export default async function LocaleLayout({params, children}: LayoutProps) {
 
     // we only have locale in the params on the [locale] level
@@ -18,10 +55,11 @@ export default async function LocaleLayout({params, children}: LayoutProps) {
 
     const bodyAttrs: { [key: string]: string } = {
         className: "edit",
-        [PORTAL_COMPONENT_ATTRIBUTE]: "page"
+        [PORTAL_COMPONENT_ATTRIBUTE]: "page",
+        ['data-theme']:"idebanken"
     }
 
-    return (<html lang="en">
+    return (<html lang="en" className={mundial.variable}>
         <body {...bodyAttrs}>
         {children}
         </body>
