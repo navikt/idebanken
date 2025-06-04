8d12e30178c006ec954fe2c0e8d039f31304b7c3
Igor Koudrik <igor.koudrik@eyeye.no>
2025-06-04 09:51:33 +0200

    Changes next.config to ts, replaced require with import, fixed any type for config, removed unused variables

---
+++ frontend/next.config.ts
@@ -0,0 +1,42 @@
+import type { NextConfig } from 'next'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function getEnonicWebpackConfig(config: NextConfig) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    fs: false,
+  };
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    "@phrases": path.resolve(__dirname, "./src/phrases"),
+  };
+  return config;
+}
+
+async function getEnonicHeaders() {
+  return [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'Content-Security-Policy',
+          value: `script-src 'self' 'unsafe-eval' 'unsafe-inline';`,
+        },
+      ],
+    },
+  ];
+}
+
+const config = {
+  output: "standalone",
+  reactStrictMode: true,
+  trailingSlash: false,
+  transpilePackages: ['@enonic/nextjs-adapter'],
+  webpack: getEnonicWebpackConfig,
+  headers: getEnonicHeaders,
+};
+
+export default config;
