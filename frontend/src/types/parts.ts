f1c2627b7ad08ee5efe504e3afd19304e91fbf23
Igor Koudrik <igor.koudrik@eyeye.no>
2025-06-12 14:14:20 +0200

    Nullable text prop in Heading

--- frontend/src/types/parts.ts
+++ frontend/src/types/parts.ts
@@ -1,9 +1,9 @@
-import { object, string, picklist, InferOutput } from 'valibot'
+import { object, string, picklist, InferOutput, nullable } from 'valibot'
 
 export const headingConfigSchema = object({
   level: picklist(['1', '2', '3', '4', '5', '6']),
   size: picklist(['xlarge', 'large', 'medium', 'small', 'xsmall']),
-  text: string(),
+  text: nullable(string()),
 })
 
 export type HeadingConfig = InferOutput<typeof headingConfigSchema>
