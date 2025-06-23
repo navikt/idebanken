25dd0a1c42090ed4aebc756082b9c01bb69f3cea
Igor Koudrik <igor.koudrik@eyeye.no>
2025-06-23 16:38:06 +0200

    Page component and footer

---
+++ frontend/src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+	return (
+		<footer className="bg-extra-light-pink">
+			<div className="container mx-auto py-4 text-center">
+				<p className="text-sm text-gray-600">© 2025 Idebanken. All rights reserved.</p>
+			</div>
+		</footer>
+	)
+}
--- frontend/src/components/parts/LinkPanel.tsx
+++ frontend/src/components/parts/LinkPanel.tsx
@@ -1,30 +1,25 @@
+/* NB: LinkPanel is depriciated in Aksel */
 'use client'
 import React from 'react'
-import { PartData } from '@enonic/nextjs-adapter';
-import { LinkPanel } from "@navikt/ds-react";
-import styles from './LinkPanel.module.css';
-import { PersonIcon } from '@navikt/aksel-icons';
+import { PartData } from '@enonic/nextjs-adapter'
+import { LinkPanel } from '@navikt/ds-react'
+import styles from './LinkPanel.module.css'
+import { PersonIcon } from '@navikt/aksel-icons'
 
 export interface LinkPanelData {
-    part: PartData;
+	part: PartData
 }
 
-export const LinkPanelView = ({part}: LinkPanelData) => {
-    console.log({part})
-    const {
-        url = '#',
-        text = '',
-    } = part?.config || {};
+export const LinkPanelView = ({ part }: LinkPanelData) => {
+	const { url = '#', text = '' } = part?.config || {}
 
-    return (
-        <LinkPanel 
-            href={url} 
-            border={false} 
-            className='bg-pink-base rounded-[100px] py-12 px-12'>
-            <LinkPanel.Title className={styles.title}>
-            {/* TODO: Lag en løsning som støtter flere ikoner */}
-                <PersonIcon fontSize="2.5rem" />
-                {text || 'Mangler lenketekst'}</LinkPanel.Title>
-        </LinkPanel>
-    )
-};
+	return (
+		<LinkPanel href={url} border={false} className="bg-pink-base rounded-[100px] py-12 px-12">
+			<LinkPanel.Title className={styles.title}>
+				{/* TODO: Lag en løsning som støtter flere ikoner */}
+				<PersonIcon fontSize="2.5rem" />
+				{text || 'Mangler lenketekst'}
+			</LinkPanel.Title>
+		</LinkPanel>
+	)
+}
