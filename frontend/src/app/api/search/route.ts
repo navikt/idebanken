298e558e7c234192093e963739d69899726846b6
Amund Lindstøl <alindstol@gmail.com>
2025-06-24 15:16:32 +0200

    set search api url

--- frontend/src/app/api/search/route.ts
+++ frontend/src/app/api/search/route.ts
@@ -1,12 +1,11 @@
-// app/api/search/route.ts
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const searchTerm = searchParams.get('ord');
-  
-  const response = await fetch(`http://localhost:9000/content/search?ord=${searchTerm}`);
-  const data = await response.json();
-  
-  return NextResponse.json(data);
+    const { searchParams } = new URL(request.url)
+    const searchTerm = searchParams.get('ord')
+
+    const response = await fetch(`${process.env.SEARCH_API_URL}/content/search?ord=${searchTerm}`)
+    const data = await response.json()
+
+    return NextResponse.json(data)
 }
