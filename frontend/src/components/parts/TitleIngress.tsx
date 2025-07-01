356b12b032eab6ee3e112c717377b8a702b5daee
ik-eyeye <84442573+ik-eyeye@users.noreply.github.com>
2025-07-01 14:43:15 +0200

    Parsing ingress HTML (#50)
    * Defined some colors vars, also in Tailwind. Tested workflow on LinkPanel component
    
    * Removed unnecessary curly brackets
    
    * Style experiments with button og heading
    
    * Styles for buttons and headings, extra button params in XP
    
    * Added button and heading config in XP. Added Valibot. Example use of Valibot for headng. Added support for tilde
    
    * Nullable text prop in Heading
    
    * New generated.d.ts
    
    * Moved validation to utils. Return null if component is not validated
    
    * Itinitial button implementation with validation
    
    * HTML processor
    
    * First html parser version
    
    * Right type for simpleTextEditor in TextEditor
    
    * Replaced old ParsedHtml
    
    * Added links to HTML-parser
    
    * Housekeeping
    
    * WIP image part
    
    * Image component
    
    * Accordion component rewrite
    
    * Accordion after fix
    
    * Bruker HeadingView i htmlParser - single source of truth
    
    * Button part type selector init commit
    
    * Query experiments, trying to get url
    
    * Button xml changes
    
    * Button as link, external or internal
    
    * Page component and footer
    
    * Use of page and pageBlock in layout. New header and footer
    
    * Formatting and small fixing
    
    * Moved image type to types/parts
    
    * ButtonView support for action buttons
    
    * Small button improvements
    
    * Two column layout with Aksel
    
    * Two column responsive layout with Aksel
    
    * Single column layout with Aksel
    
    * Singe and 2-column layout with Aksel and better typing
    
    * Better lable for panel-2-column def
    
    * Panel layout 2 columns with Aksel and types
    
    * Deleted unnecessary module css
    
    * Fix for logo dimentions
    
    * Parsing ingress
    
    * Parsing html page data
    
    ---------
    
    Co-authored-by: Amund Lindstøl <alindstol@gmail.com>

--- frontend/src/components/parts/TitleIngress.tsx
+++ frontend/src/components/parts/TitleIngress.tsx
@@ -1,10 +1,16 @@
-import { BodyShort, Heading } from '@navikt/ds-react'
+import { Heading } from '@navikt/ds-react'
 import { CommonType } from '../queries/common'
-import { Idebanken_Guide_Data, Part_Idebanken_Heading } from '~/types/generated.d'
+import { Part_Idebanken_Heading } from '~/types/generated.d'
+import { parseHtml } from '~/utils/parseHtml'
+
+type PageData = {
+	ingress: string
+	title: string
+}
 
 export interface TitleIngressData {
 	part: { descriptor?: string; config: Part_Idebanken_Heading }
-	common?: CommonType<Idebanken_Guide_Data>
+	common?: CommonType<PageData>
 }
 
 const TitleIngressView = ({ common }: TitleIngressData) => {
@@ -13,15 +19,7 @@ const TitleIngressView = ({ common }: TitleIngressData) => {
 			<Heading level={'1'} size={'xlarge'}>
 				{common?.get?.dataAsJson?.title || '[Mangler tittel på innholdet]'}
 			</Heading>
-			{/* @ts-expect-error children is required */}
-			<BodyShort
-				size="large"
-				className="font-light"
-				dangerouslySetInnerHTML={{
-					__html: common?.get?.dataAsJson?.ingress || '[Mangler ingress på innholdet]',
-				}}
-				spacing
-			/>
+			<>{parseHtml(common?.get?.dataAsJson?.ingress || '[Mangler ingress]')}</>
 		</>
 	)
 }
