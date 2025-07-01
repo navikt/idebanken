8858210fd6c5227fc7c214b9512e02cd9fa58151
Igor Koudrik <igor.koudrik@eyeye.no>
2025-07-01 14:31:06 +0200

    Parsing ingress

--- frontend/src/components/parts/TitleIngress.tsx
+++ frontend/src/components/parts/TitleIngress.tsx
@@ -1,10 +1,16 @@
 import { BodyShort, Heading } from '@navikt/ds-react'
 import { CommonType } from '../queries/common'
-import { Idebanken_Guide_Data, Part_Idebanken_Heading } from '~/types/generated.d'
+import { Part_Idebanken_Heading } from '~/types/generated.d'
+import { parseHtml } from '~/utils/parseHtml'
+
+type textEditorData = {
+	ingress: string
+	title: string
+}
 
 export interface TitleIngressData {
 	part: { descriptor?: string; config: Part_Idebanken_Heading }
-	common?: CommonType<Idebanken_Guide_Data>
+	common?: CommonType<textEditorData>
 }
 
 const TitleIngressView = ({ common }: TitleIngressData) => {
@@ -13,15 +19,9 @@ const TitleIngressView = ({ common }: TitleIngressData) => {
 			<Heading level={'1'} size={'xlarge'}>
 				{common?.get?.dataAsJson?.title || '[Mangler tittel på innholdet]'}
 			</Heading>
-			{/* @ts-expect-error children is required */}
-			<BodyShort
-				size="large"
-				className="font-light"
-				dangerouslySetInnerHTML={{
-					__html: common?.get?.dataAsJson?.ingress || '[Mangler ingress på innholdet]',
-				}}
-				spacing
-			/>
+			<BodyShort size="large" className="font-light">
+				{parseHtml(common?.get?.dataAsJson?.ingress || '')}
+			</BodyShort>
 		</>
 	)
 }
