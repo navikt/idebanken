c4a94d987972da99e4787569ed14c6e181157469
ik-eyeye <84442573+ik-eyeye@users.noreply.github.com>
2025-07-01 13:43:01 +0200

    Panel Layout 2 columns with Aksel (#49)
    * Defined some colors vars, also in Tailwind. Tested workflow on LinkPanel component
    
    * Removed unnecessary curly brackets
    
    * Style experiments with button og heading
    
    * Styles for buttons and headings, extra button params in XP
    
    * Added button and heading config in XP. Added Valibot. Example use of Valibot for headng. Added support for tilde
    
    * Nullable text prop in Heading
    
    * New generated.d.ts
    
    * Moved validation to utils. Return null if component is not validated
    
    * Itinitial button implementation with validation
    
    * HTML processor
    
    * First html parser version
    
    * Right type for simpleTextEditor in TextEditor
    
    * Replaced old ParsedHtml
    
    * Added links to HTML-parser
    
    * Housekeeping
    
    * WIP image part
    
    * Image component
    
    * Accordion component rewrite
    
    * Accordion after fix
    
    * Bruker HeadingView i htmlParser - single source of truth
    
    * Button part type selector init commit
    
    * Query experiments, trying to get url
    
    * Button xml changes
    
    * Button as link, external or internal
    
    * Page component and footer
    
    * Use of page and pageBlock in layout. New header and footer
    
    * Formatting and small fixing
    
    * Moved image type to types/parts
    
    * ButtonView support for action buttons
    
    * Small button improvements
    
    * Two column layout with Aksel
    
    * Two column responsive layout with Aksel
    
    * Single column layout with Aksel
    
    * Singe and 2-column layout with Aksel and better typing
    
    * Better lable for panel-2-column def
    
    * Panel layout 2 columns with Aksel and types
    
    * Deleted unnecessary module css
    
    ---------
    
    Co-authored-by: Amund Lindstøl <alindstol@gmail.com>

--- frontend/src/components/layouts/PanelLayoutTwoColumn.tsx
+++ frontend/src/components/layouts/PanelLayoutTwoColumn.tsx
@@ -1,43 +1,74 @@
-import type { LayoutProps } from '@enonic/nextjs-adapter'
+import type { Layout_Idebanken_Panel_2_Column } from '~/types/generated.d'
+import type { PageComponent, MetaData } from '@enonic/nextjs-adapter'
+import type { CommonType } from '../queries/common'
 import { RegionView } from '@enonic/nextjs-adapter/views/Region'
-import styles from './PanelLayoutTwoColumn.module.css'
-import classNames from 'classnames'
+import { Box, HGrid } from '@navikt/ds-react'
 
-const PanelLayoutTwoColumn = (props: LayoutProps) => {
+interface PanelLayoutTwoColumnProps {
+	layout: {
+		config: Layout_Idebanken_Panel_2_Column
+		descriptor: string
+		regions: {
+			left: {
+				components: PageComponent[]
+			}
+			right: {
+				components: PageComponent[]
+			}
+		}
+	}
+	common: CommonType
+	meta: MetaData
+}
+
+const PanelLayoutTwoColumn = (props: PanelLayoutTwoColumnProps) => {
 	const regions = props.layout.regions
 	const { common, meta, layout } = props
+	const leftSpan = Number(layout.config?.leftSpan) || 6
+	const rightSpan = 12 - leftSpan
 
-	// console.log('PanelLayoutTwoColumn', { layout })
-
-	const [bg1, bg2] = layout.config?.background || []
+	const [bgOuterBox, bgInnerBox] = layout.config?.background || []
 
 	return (
-		<div
-			className={bg1?.bgColor || 'bg-extra-light-pink'}
-			style={
-				{ [`--left-span`]: Number(layout.config?.leftSpan || 6) + 1 } as React.CSSProperties
-			}>
-			<div className={styles.wrapper}>
-				<div className={classNames(styles.panel, bg2?.bgColor || 'bg-extra-light-pink')}>
-					<div className={styles.left}>
-						<RegionView
-							name="left"
-							components={regions['left']?.components}
-							common={common}
-							meta={meta}
-						/>
-					</div>
-					<div className={styles.right}>
-						<RegionView
-							name="right"
-							components={regions['right']?.components}
-							common={common}
-							meta={meta}
-						/>
-					</div>
+		<Box
+			as="section"
+			padding={{
+				xs: 'space-8',
+				sm: 'space-12',
+				md: 'space-16',
+				lg: 'space-20',
+				xl: 'space-24',
+			}}
+			className={bgOuterBox?.bgColor || 'bg-extra-light-pink'}>
+			<HGrid
+				gap={{ sm: 'space-8', md: 'space-12', lg: 'space-20', xl: 'space-24' }}
+				columns={{ xs: 1, md: 12 }}
+				padding={{
+					xs: 'space-12',
+					sm: 'space-16',
+					md: 'space-20',
+					lg: 'space-24',
+					xl: 'space-32',
+				}}
+				className={`${bgInnerBox?.bgColor || 'bg-extra-light-pink'} rounded-3xl`}>
+				<div className={`col-span-1 md:col-span-${leftSpan}`}>
+					<RegionView
+						name="left"
+						components={regions['left']?.components}
+						common={common}
+						meta={meta}
+					/>
 				</div>
-			</div>
-		</div>
+				<div className={`col-span-1 md:col-span-${rightSpan}`}>
+					<RegionView
+						name="right"
+						components={regions['right']?.components}
+						common={common}
+						meta={meta}
+					/>
+				</div>
+			</HGrid>
+		</Box>
 	)
 }
 
