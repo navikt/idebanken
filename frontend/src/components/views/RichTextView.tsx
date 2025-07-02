00773c3e7e0ab1103952ba6192d1400ca58151a9
Amund Lindstøl <alindstol@gmail.com>
2025-07-02 14:23:44 +0200

    copy of RichTextView

---
+++ frontend/src/components/views/RichTextView.tsx
@@ -0,0 +1,93 @@
+import type {
+	ImageComponentParams,
+	LinkComponentParams,
+	MacroComponentParams,
+	Replacer as NewReplacer,
+} from '@enonic/react-components'
+import { RichText } from '@enonic/react-components'
+import type { DOMNode } from 'html-react-parser'
+import {
+	getUrl,
+	MacroConfig,
+	MetaData,
+	Replacer as OldReplacer,
+	RichTextData,
+	RichTextViewProps,
+	sanitizeGraphqlName,
+	UrlProcessor,
+} from '@enonic/nextjs-adapter'
+import BaseMacro from '@enonic/nextjs-adapter/views/BaseMacro'
+import { Link } from '@navikt/ds-react'
+
+interface ExtraRichTextProps {
+	meta: MetaData
+	renderInEditMode?: boolean
+}
+
+const RichTextView = (props: RichTextViewProps) => {
+	return (
+		<RichText<ExtraRichTextProps>
+			data={props.data}
+			meta={props.meta}
+			className={props.className}
+			tag={props.tag}
+			replacer={wrapReplacer(props.customReplacer, props.meta, props.renderMacroInEditMode)}
+			renderInEditMode={props.renderMacroInEditMode ?? true}
+			Macro={MacroAdapter}
+			Link={LinkAdapter}
+			Image={ImageAdapter}
+		/>
+	)
+}
+
+function wrapReplacer(
+	oldReplacer: OldReplacer | undefined,
+	meta: MetaData,
+	renderMacroInEditMode: boolean
+): NewReplacer {
+	if (!oldReplacer) {
+		return null
+	}
+	return (element: DOMNode, data: RichTextData) => {
+		return oldReplacer(element, data, meta, renderMacroInEditMode)
+	}
+}
+
+function MacroAdapter(props: MacroComponentParams<ExtraRichTextProps>) {
+	const { children, descriptor, config, meta, renderInEditMode } = props
+	const name = descriptor.substring(descriptor.indexOf(':') + 1)
+	const data = {
+		name: name,
+		descriptor: descriptor,
+		config: {
+			[sanitizeGraphqlName(name)]: config as Record<string, MacroConfig>,
+		},
+	}
+
+	return (
+		<BaseMacro data={data} meta={meta} renderInEditMode={renderInEditMode}>
+			{children}
+		</BaseMacro>
+	)
+}
+
+function LinkAdapter(props: LinkComponentParams<ExtraRichTextProps>) {
+	return <Link href={getUrl(props.href, props.meta)}>{props.children}</Link>
+}
+
+function ImageAdapter(props: ImageComponentParams<ExtraRichTextProps>) {
+	const srcSet = props.srcSet?.length
+		? UrlProcessor.processSrcSet(props.srcSet, props.meta)
+		: undefined
+	return (
+		<img
+			src={getUrl(props.src, props.meta)}
+			style={props.style}
+			alt={props.alt}
+			sizes={props.sizes}
+			srcSet={srcSet}
+		/>
+	)
+}
+
+export default RichTextView
