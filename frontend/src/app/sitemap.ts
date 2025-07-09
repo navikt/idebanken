99a1b0dc350674c9d91bcc23d17550ea6995b66e
Amund Lindstøl <alindstol@gmail.com>
2025-07-09 11:17:32 +0200

    added sitemap.xml

---
+++ frontend/src/app/sitemap.ts
@@ -0,0 +1,52 @@
+import { MetadataRoute } from 'next'
+import { getContentApiUrl } from '@enonic/nextjs-adapter/src/utils/getContentApiUrl'
+import process from 'node:process'
+import { fetchGuillotine } from '@enonic/nextjs-adapter/server'
+import { Sitemap } from '~/types/generated'
+import { forceArray } from '~/utils/utils'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const res = await fetchGuillotine(
+        getContentApiUrl({ contentPath: process.env.ENONIC_API ?? '' }),
+        {
+            site: '/idebanken',
+            default: true,
+            locale: 'no',
+            project: 'idebanken',
+        },
+        {
+            method: 'POST',
+            body: {
+                query: `{
+                    guillotine {
+                        sitemap {
+                            baseUrl
+                            urlset {
+                                lastmod
+                                changefreq
+                                lastmod
+                                path
+                                priority
+                            }
+                        }
+                    }
+                }`,
+            },
+        }
+    )
+
+    const sitemap = res.guillotine.sitemap
+    if (!sitemap) {
+        console.warn('Sitemap not found or empty')
+        return []
+    }
+
+    const { baseUrl, urlset } = sitemap as Sitemap
+    return forceArray(urlset).map((set) => ({
+        url: `${baseUrl ?? ''}${set?.path}`,
+        lastModified: new Date(set?.lastmod),
+        changeFrequency: (set?.changefreq ??
+            'monthly') as MetadataRoute.Sitemap[0]['changeFrequency'],
+        priority: set?.priority ? Number(set?.priority) : 0.5,
+    }))
+}
