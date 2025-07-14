b2b9fbb353a2c75e928b1e69b06d8b328ee042d3
Amund Lindstøl <alindstol@gmail.com>
2025-07-14 11:44:33 +0200

    parse props before component rendering

--- frontend/src/components/parts/Image.tsx
+++ frontend/src/components/parts/Image.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import { forceArray } from '~/utils/utils'
-import { getUrl, MetaData } from '@enonic/nextjs-adapter'
+import { getUrl } from '@enonic/nextjs-adapter'
 import { PartData } from '~/types/graphql-types'
 import { Part_Idebanken_Image, Part_Idebanken_Image_Circles } from '~/types/generated'
 
@@ -14,88 +14,134 @@ export type ImageData = {
     }
 }
 
+interface BasicImageProps {
+    src: string
+    alt?: string | null
+    width: number
+    height: number
+    className?: string
+}
+
+interface StyledImageProps extends BasicImageProps {
+    borderRadius?: number
+    showBorder: boolean
+    borderDistance: number
+    circles: Array<{
+        size: number
+        color: string
+        bottom: number
+        left: number
+        opacity: number
+    }>
+}
+
+const accentColors: Record<string, string> = {
+    pink: 'bg-pink-500',
+    red: 'bg-red-500',
+    blue: 'bg-dark-blue-500',
+}
+
 export const ImageView = (props: PartData<Part_Idebanken_Image & ImageData>) => {
     const { part, meta } = props
     const { image } = part.config
-    const { config } = part
-    console.log('image config', config)
 
+    const { config } = part
+    const scale = config.scale ? Number(config.scale) / 100 : 1
+    const width1 = config.width ? Number(config.width) : 440
+    const height1 = config.height ? Number(config.height) : 675
+    const scaledWidth = Math.round(Number(width1) * scale)
+    const scaledHeight = Math.round(Number(height1) * scale)
+
+    const parsedProps = {
+        imageUrl:
+            scaledWidth && scaledHeight
+                ? getUrl(image.imageUrl, meta)?.replace(
+                      /(\/_\/image\/[^/]+)\/([^/]+)/,
+                      `$1/block-${scaledWidth}-${scaledHeight}`
+                  )
+                : image.imageUrl,
+        width: width1,
+        height: height1,
+        scale,
+        showBorder: config.border ?? false,
+        borderRadius: config.borderRadius ? Number(config.borderRadius) : 0,
+        borderDistance: config.borderDistance ? Number(config.borderDistance) : 0,
+        circles: forceArray(config.circles as Part_Idebanken_Image_Circles[]).map((circle) => ({
+            size: circle.size ? Number(circle.size) : 200,
+            color: circle.color ? accentColors[circle.color] : accentColors.pink,
+            bottom: circle.bottom ? Number(circle.bottom) : -50,
+            left: circle.left ? Number(circle.left) : -90,
+            opacity: circle.opacity ? Number(circle.opacity) / 100 : 0.8,
+        })),
+    }
     return (
         <>
             {config.styleActive ? (
                 <StyledImage
-                    src={image.imageUrl}
+                    src={parsedProps.imageUrl}
                     alt={image.data.altText}
-                    scale={config.scale}
-                    width={config.width}
-                    height={config.height}
-                    borderRadius={config.borderRadius}
-                    showBorder={config.border}
-                    borderDistance={config.borderDistance}
-                    circles={forceArray(config.circles) as Part_Idebanken_Image_Circles[]}
-                    meta={meta}
-                    className=""
+                    width={parsedProps.width}
+                    height={parsedProps.height}
+                    borderRadius={parsedProps.borderRadius}
+                    showBorder={parsedProps.showBorder}
+                    borderDistance={parsedProps.borderDistance}
+                    circles={parsedProps.circles}
                 />
             ) : (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img src={image.imageUrl} alt={image.data.altText ?? 'Bilde'} />
+                <BasicImage
+                    src={parsedProps.imageUrl}
+                    alt={image.data.altText}
+                    width={parsedProps.width}
+                    height={parsedProps.height}
+                />
             )}
         </>
     )
 }
 
-interface StyledImageProps {
-    src: string
-    alt?: string | null
-    width?: string | null
-    height?: string | null
-    borderRadius?: string | null
-    showBorder?: boolean | null
-    borderDistance?: string | null
-    scale?: string | null
-    circles?: Array<{
-        size?: string | null
-        color?: string | null
-        bottom?: string | null
-        left?: string | null
-        opacity?: string | null
-    }> | null
-    className?: string
-    meta?: MetaData
+function BasicImage({ src, alt, width, height, className }: Readonly<BasicImageProps>) {
+    return (
+        <div className={classNames('relative inline-block', className)}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+                src={src}
+                alt={alt ?? 'Bilde'}
+                className="object-cover"
+                style={{
+                    width: `${width}px`,
+                    height: `${height}px`,
+                }}
+                loading="lazy"
+                decoding="async"
+            />
+        </div>
+    )
 }
 
 function StyledImage({
     src,
-    alt = '#',
-    width = '440',
-    height = '675',
-    borderRadius = '9999',
-    showBorder = false,
-    borderDistance = '40',
+    alt,
+    width,
+    height,
+    borderRadius,
+    showBorder,
+    borderDistance,
     circles = [],
     className,
-    meta,
-    scale = '1',
 }: Readonly<StyledImageProps>) {
-    const accentColors = {
-        pink: 'bg-pink-500',
-        red: 'bg-red-500',
-        blue: 'bg-dark-blue-500',
-    }
-    const borderDist = borderDistance ? Number(borderDistance) : 0
-    const scaleFactor = scale ? Number(scale) / 100 : 1
+    const borderDist = borderDistance || 0
     return (
         <div
             className={classNames('relative inline-block', className)}
-            style={{ margin: `${Number(borderDist) / 2}px` }}>
+            style={{ margin: `${Math.round(borderDist / 2)}px` }}>
             {showBorder && (
                 <div
                     className={classNames('absolute -inset-3 border-1 border-[#0000004D]')}
                     style={{
-                        width: Number(width) + Number(borderDist),
-                        height: Number(height) + Number(borderDist),
-                        left: -(Number(borderDist) / 2),
-                        top: -(Number(borderDist) / 2),
+                        width: `${width + borderDist}px`,
+                        height: `${height + borderDist}px`,
+                        left: -Math.round(borderDist / 2),
+                        top: -Math.round(borderDist / 2),
                         borderRadius: `${borderRadius}px`,
                     }}
                 />
@@ -108,13 +154,11 @@ function StyledImage({
                     height: `${height}px`,
                     borderRadius: `${borderRadius}px`,
                 }}>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                     loading="lazy"
                     decoding="async"
-                    src={getUrl(src, meta)?.replace(
-                        /(\/_\/image\/[^/]+)\/([^/]+)/,
-                        `$1/block-${Math.floor(Number(width) * Number(scaleFactor))}-${Math.floor(Number(height) * Number(scaleFactor))}`
-                    )}
+                    src={src}
                     alt={alt}
                     width={'100%'}
                     height={'100%'}
@@ -127,17 +171,14 @@ function StyledImage({
             {circles?.map(({ size, color, bottom, left, opacity }, id) => (
                 <div
                     key={id}
-                    className={classNames(
-                        `absolute rounded-full `,
-                        accentColors[color] || accentColors.pink
-                    )}
+                    className={classNames(`absolute rounded-full `, color)}
                     style={{
                         width: `${size}px`,
                         height: `${size}px`,
-                        bottom: bottom ? `${bottom}px` : '-50px',
-                        left: left ? `${left}px` : '-90px',
+                        bottom: `${bottom}px`,
+                        left: `${left}px`,
                         zIndex: id + 1,
-                        opacity: opacity ? Number(opacity) / 100 : 0.8,
+                        opacity: opacity,
                     }}
                 />
             ))}
