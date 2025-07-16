fe0107c626148d05538d3376a06fba163f4a3e6f
Amund Lindstøl <alindstol@gmail.com>
2025-07-16 09:41:38 +0200

    auto format

--- frontend/src/components/common/InfoBox.tsx
+++ frontend/src/components/common/InfoBox.tsx
@@ -1,6 +1,6 @@
 import { Info } from '~/components/icons/Info'
 
-export interface InfoBoxProps {
+interface InfoBoxProps {
     children: React.ReactNode
     bgColorClass?: string
 }
@@ -12,18 +12,18 @@ export const InfoBox = ({ children, bgColorClass = 'bg-extra-light-pink' }: Info
                 filter: 'drop-shadow(0px 24px 44px rgba(88, 84, 106, 0.30))',
             }}
             className="
-				relative 
+				relative
 				pt-4 pr-4
 				flex-1
 			">
             <div
                 className="
-					bg-dark-blue-500 
-					h-16 w-16 
-					rounded-full 
-					absolute 
-					top-0 right-0 
-					z-1 
+					bg-dark-blue-500
+					h-16 w-16
+					rounded-full
+					absolute
+					top-0 right-0
+					z-1
 					flex justify-center items-center
 				">
                 <Info />
@@ -33,9 +33,9 @@ export const InfoBox = ({ children, bgColorClass = 'bg-extra-light-pink' }: Info
                     mask: 'radial-gradient(circle 1px at calc(100% - 16px) calc(0% + 16px), transparent 39px, purple 0)',
                 }}
                 className={`
-					${bgColorClass} 
-					py-16 px-[1.5rem] 
-					rounded-[72px] 
+					${bgColorClass}
+					py-16 px-[1.5rem]
+					rounded-[72px]
 				`}>
                 {children}
             </div>
