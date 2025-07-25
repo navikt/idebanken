6811b1ca6676ab60b81b48d91a3780c01e5591c1
Amund Lindstøl <alindstol@gmail.com>
2025-07-25 15:16:14 +0200

    WIP crash course

--- frontend/src/components/contentType/CrashCourse.tsx
+++ frontend/src/components/contentType/CrashCourse.tsx
@@ -1,10 +1,16 @@
 import type { FetchContentResult } from '@enonic/nextjs-adapter/src/types'
 import { getCrashCourseSlideContents } from '~/components/queries/crash-course'
 import CrashCourseView from '~/components/views/CrashCourseView'
+import MainView from '@enonic/nextjs-adapter/views/MainView'
 
 export default async function CrashCourse(props: FetchContentResult) {
     const slideContents = await getCrashCourseSlideContents(props)
-    console.log('CrashCourse data:', slideContents)
 
-    return <CrashCourseView slideContents={slideContents} />
+    return (
+        <CrashCourseView
+            slideDeckElements={slideContents.map((slide, i) => (
+                <MainView key={i} {...slide} />
+            ))}
+        />
+    )
 }
--- frontend/src/components/views/CrashCourseView.tsx
+++ frontend/src/components/views/CrashCourseView.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import { useState } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
-import MainView from '@enonic/nextjs-adapter/views/MainView'
+import React, { type JSX, useState } from 'react'
+import { AnimatePresence, motion, Variants } from 'framer-motion'
 import styles from './CrashCourse.module.css'
 
-export default function CrashCourseView({ slideContents }) {
+export default function CrashCourseView({
+    slideDeckElements,
+}: {
+    slideDeckElements: JSX.Element[]
+}) {
     const [currentIndex, setCurrentIndex] = useState(0)
     const [direction, setDirection] = useState(1) // 1 for forward, -1 for backward
 
     const goToNextSlide = () => {
-        if (currentIndex < slideContents.length - 1) {
+        if (currentIndex < slideDeckElements.length - 1) {
             setDirection(1)
             setCurrentIndex(currentIndex + 1)
         }
@@ -23,13 +26,13 @@ export default function CrashCourseView({ slideContents }) {
         }
     }
 
-    const variants = {
-        enter: (direction) => ({
+    const variants: Variants = {
+        enter: (direction: number) => ({
             x: direction > 0 ? '100%' : '-100%',
             opacity: 0,
         }),
         center: { x: 0, opacity: 1 },
-        exit: (direction) => ({
+        exit: (direction: number) => ({
             x: direction < 0 ? '100%' : '-100%',
             opacity: 0,
         }),
@@ -37,7 +40,7 @@ export default function CrashCourseView({ slideContents }) {
 
     return (
         <div className={styles.slideContainer}>
-            <AnimatePresence initial={false} custom={direction} mode="wait">
+            <AnimatePresence initial={false} custom={direction} mode="sync">
                 <motion.div
                     key={currentIndex}
                     custom={direction}
@@ -46,7 +49,7 @@ export default function CrashCourseView({ slideContents }) {
                     animate="center"
                     exit="exit"
                     transition={{ type: 'tween', duration: 0.5 }}>
-                    <MainView {...slideContents[currentIndex]} />
+                    {slideDeckElements[currentIndex]}
                 </motion.div>
             </AnimatePresence>
 
@@ -55,11 +58,11 @@ export default function CrashCourseView({ slideContents }) {
                     Previous
                 </button>
                 <span>
-                    {currentIndex + 1} / {slideContents.length}
+                    {currentIndex + 1} / {slideDeckElements.length}
                 </span>
                 <button
                     onClick={goToNextSlide}
-                    disabled={currentIndex === slideContents.length - 1}>
+                    disabled={currentIndex === slideDeckElements.length - 1}>
                     Next
                 </button>
             </div>
