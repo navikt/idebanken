9ef2efb90a391ff5e189122463df18c16f878ce4
Amund Lindstøl <alindstol@gmail.com>
2025-09-08 16:25:11 +0200

    Added search button menu and changed the html structure

--- frontend/src/components/views/Header.tsx
+++ frontend/src/components/views/Header.tsx
@@ -8,7 +8,7 @@ import { SearchWrapper } from '~/components/common/SearchWrapper'
 import { HeadlessCms } from '~/types/generated'
 import { Bleed, Box, Button, HGrid, HStack, VStack } from '@navikt/ds-react'
 import { HeadingView } from '~/components/parts/Heading'
-import { MenuHamburgerIcon, XMarkIcon } from '@navikt/aksel-icons'
+import { MagnifyingGlassIcon, MenuHamburgerIcon, XMarkIcon } from '@navikt/aksel-icons'
 import { useState } from 'react'
 import { PageBlock } from '@navikt/ds-react/Page'
 import classNames from 'classnames'
@@ -22,15 +22,16 @@ export interface HeaderProps {
 
 const Header = ({ title, logoUrl, header }: HeaderProps) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isSearchOpen, setIsSearchOpen] = useState(false)
 
     return (
         <>
             <BleedingBackgroundPageBlock
                 as="header"
                 width={'2xl'}
-                bgColor="bg-pink-100"
-                className="relative"
-                customClassNames="shadow-[0_-1px_0_0_#CFCFCF_inset] relative z-40">
+                // bgColor="bg-pink-100"
+                className="relative items-center"
+                bleedClassName="shadow-[0_-1px_0_0_#CFCFCF_inset] relative z-40 overflow-y-visible">
                 <HStack
                     align="center"
                     justify="space-between"
@@ -46,76 +47,129 @@ const Header = ({ title, logoUrl, header }: HeaderProps) => {
                             priority
                         />
                     </NextLink>
-
-                    <Button
-                        variant="tertiary"
-                        aria-label={isMenuOpen ? 'Lukk meny' : 'Åpne meny'}
-                        onClick={() => setIsMenuOpen(!isMenuOpen)}
-                        className="[&_.navds-label]:translate-y-[2px] flex-col sm:flex-row"
-                        icon={
-                            <div
-                                className={`transition-transform duration-200 ${isMenuOpen ? 'rotate-180' : ''}`}>
-                                {isMenuOpen ? (
-                                    <XMarkIcon aria-hidden />
-                                ) : (
-                                    <MenuHamburgerIcon aria-hidden />
-                                )}
-                            </div>
-                        }>
-                        Meny
-                    </Button>
+                    <HStack gap="2">
+                        <Button
+                            variant="tertiary"
+                            aria-label={isMenuOpen ? 'Lukk meny' : 'Åpne meny'}
+                            onClick={() => {
+                                setIsMenuOpen(!isMenuOpen)
+                                setIsSearchOpen(false)
+                            }}
+                            className="[&_.navds-label]:translate-y-[2px] flex-col sm:flex-row"
+                            icon={
+                                <div
+                                    className={`transition-transform duration-200 ${isMenuOpen ? 'rotate-90' : ''}`}>
+                                    {isMenuOpen ? (
+                                        <XMarkIcon aria-hidden />
+                                    ) : (
+                                        <MenuHamburgerIcon aria-hidden />
+                                    )}
+                                </div>
+                            }>
+                            Meny
+                        </Button>
+                        <Button
+                            variant={'tertiary'}
+                            aria-label={isSearchOpen ? 'Lukk søk' : 'Åpne søk'}
+                            onClick={() => {
+                                setIsSearchOpen(!isSearchOpen)
+                                setIsMenuOpen(false)
+                            }}
+                            className={'[&_.navds-label]:translate-y-[2px] flex-col sm:flex-row'}
+                            icon={
+                                <div
+                                    className={`transition-transform duration-200 ${isSearchOpen ? 'rotate-90' : ''}`}>
+                                    {isSearchOpen ? (
+                                        <XMarkIcon aria-hidden />
+                                    ) : (
+                                        <MagnifyingGlassIcon aria-hidden />
+                                    )}
+                                </div>
+                            }>
+                            Søk
+                        </Button>
+                    </HStack>
                 </HStack>
+
+                {/* Moved inside BleedingBackgroundPageBlock and positioned relative to header */}
+                <PageBlock
+                    className={classNames(
+                        'absolute',
+                        'top-full',
+                        'right-0',
+                        'transform',
+                        'bg-light-pink',
+                        'z-40',
+                        'transition-all duration-300',
+                        'rounded-b-4xl',
+                        isMenuOpen
+                            ? 'opacity-100 translate-y-0'
+                            : 'opacity-0 -translate-y-3 pointer-events-none h-0 overflow-hidden'
+                    )}
+                    aria-hidden={!isMenuOpen}
+                    inert={!isMenuOpen}
+                    width={'2xl'}
+                    gutters>
+                    <HGrid columns={{ xs: 1, md: 12 }} gap={'space-20'} className={'py-12'}>
+                        <SearchWrapper className={'col-span-1 md:col-span-4 md:order-2'} />
+                        <Box
+                            as={'nav'}
+                            className={
+                                'col-span-1 md:col-span-8 md:order-1 flex flex-wrap gap-x-20 gap-y-6'
+                            }>
+                            {header?.map(({ title, links }, id) => (
+                                <VStack key={id} gap={'2'}>
+                                    <HeadingView key={id} level={'2'} size={'xsmall'}>
+                                        {title}
+                                    </HeadingView>
+                                    {links?.map(({ linkText, href }, id) => (
+                                        <NextLink
+                                            key={id}
+                                            href={href ?? '/'}
+                                            className="underline hover:no-underline">
+                                            {linkText}
+                                        </NextLink>
+                                    ))}
+                                </VStack>
+                            ))}
+                        </Box>
+                    </HGrid>
+                </PageBlock>
+
+                <PageBlock
+                    className={classNames(
+                        'absolute',
+                        'top-full',
+                        'right-0',
+                        'transform',
+                        'bg-light-pink',
+                        'z-40',
+                        'transition-all duration-300',
+                        'rounded-b-4xl',
+                        isSearchOpen
+                            ? 'opacity-100 translate-y-0'
+                            : 'opacity-0 -translate-y-3 pointer-events-none h-0 overflow-hidden'
+                    )}
+                    aria-hidden={!isSearchOpen}
+                    inert={!isSearchOpen}
+                    width={'md'}
+                    gutters>
+                    <VStack gap={'space-20'} className={'py-12'} maxWidth={'xs'} maxHeight={'xs'}>
+                        <SearchWrapper className={''} isSearchOpen={isSearchOpen} />
+                    </VStack>
+                </PageBlock>
             </BleedingBackgroundPageBlock>
-            <PageBlock
-                className={classNames(
-                    'absolute',
-                    'bg-light-pink',
-                    'z-40',
-                    'left-1/2',
-                    'transform',
-                    '-translate-x-1/2',
-                    'transition-all duration-300',
-                    'rounded-b-4xl',
-                    isMenuOpen
-                        ? 'opacity-100 translate-y-0'
-                        : 'opacity-0 -translate-y-10 pointer-events-none h-0 overflow-hidden'
-                )}
-                aria-hidden={!isMenuOpen}
-                width={'2xl'}
-                gutters>
-                <HGrid columns={{ xs: 1, md: 12 }} gap={'space-20'} className={'py-12'}>
-                    <SearchWrapper className={'col-span-1 md:col-span-4 md:order-2'} />
-                    <Box
-                        as={'nav'}
-                        className={
-                            'col-span-1 md:col-span-8 md:order-1 flex flex-wrap gap-x-20 gap-y-6'
-                        }>
-                        {header?.map(({ title, links }, id) => (
-                            <VStack key={id} gap={'2'}>
-                                <HeadingView key={id} level={'2'} size={'xsmall'}>
-                                    {title}
-                                </HeadingView>
-                                {links?.map(({ linkText, href }, id) => (
-                                    <NextLink
-                                        key={id}
-                                        href={href ?? '/'}
-                                        className="underline hover:no-underline">
-                                        {linkText}
-                                    </NextLink>
-                                ))}
-                            </VStack>
-                        ))}
-                    </Box>
-                </HGrid>
-            </PageBlock>
 
             {/* Overlay to darken the page */}
             <Bleed
                 className={classNames(
-                    'fixed inset-x-0 top-24 md:top-20 bottom-0 bg-black transition-opacity duration-300 z-30',
-                    isMenuOpen ? 'opacity-30' : 'opacity-0 pointer-events-none'
+                    'absolute inset-x-0 top-24 md:top-20 bottom-0 bg-black transition-opacity duration-300 z-30',
+                    isMenuOpen || isSearchOpen ? 'opacity-30' : 'opacity-0 pointer-events-none'
                 )}
-                onClick={() => setIsMenuOpen(false)}></Bleed>
+                onClick={() => {
+                    setIsMenuOpen(false)
+                    setIsSearchOpen(false)
+                }}></Bleed>
         </>
     )
 }
