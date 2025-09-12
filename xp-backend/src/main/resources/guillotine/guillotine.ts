bf68130626370ff54d9d856c893e1c84e3819a3c
Amund Lindstøl <alindstol@gmail.com>
2025-09-12 15:22:09 +0200

    created link-card-list extension

--- xp-backend/src/main/resources/guillotine/guillotine.ts
+++ xp-backend/src/main/resources/guillotine/guillotine.ts
@@ -5,26 +5,31 @@ import { commonGuillotineTypes } from './common-guillotine-types'
 import { headlessCmsExtensions } from './extensions/headless-cms'
 import { categoryExtensions } from './extensions/category'
 import { tableOfContentsExtensions } from './extensions/table-of-contents'
+import { linkCardListExtensions } from './extensions/link-card-list'
 
 export function extensions(graphQL: GraphQL): Extensions {
     const headlessCms = headlessCmsExtensions(graphQL)
     const category = categoryExtensions(graphQL)
     const tableOfContents = tableOfContentsExtensions(graphQL)
+    const linkCardList = linkCardListExtensions(graphQL)
 
     return {
         types: {
             ...commonGuillotineTypes(graphQL),
             ...headlessCms.types,
+            ...linkCardList.types,
         },
         resolvers: {
             ...headlessCms.resolvers,
             ...category.resolvers,
             ...tableOfContents.resolvers,
+            ...linkCardList.resolvers,
         },
         creationCallbacks: {
             ...headlessCms.creationCallbacks,
             ...category.creationCallbacks,
             ...tableOfContents.creationCallbacks,
+            ...linkCardList.creationCallbacks,
         },
     }
 }
