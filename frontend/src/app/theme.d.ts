4b70c2b2e491ec0902700fbbb208102a5ced3fc7
Igor Koudrik <igor.koudrik@eyeye.no>
2025-09-23 16:56:28 +0200

    Typescript setting for themes and custom brands

---
+++ frontend/src/app/theme.d.ts
@@ -0,0 +1,8 @@
+import type {} from '@navikt/core/react/types/theme'
+
+declare module '@navikt/ds-react/types/theme' {
+    export interface CustomAkselColor {
+        'ib-brand-pink': never
+        'ib-brand-dark-blue': never
+    }
+}
