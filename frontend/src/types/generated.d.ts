2a948a932f0de1c62770e35408413d773dc47be8
Igor Koudrik <igor.koudrik@eyeye.no>
2025-09-23 14:51:01 +0200

    The right push

--- frontend/src/types/generated.d.ts
+++ frontend/src/types/generated.d.ts
@@ -484,7 +484,6 @@ export type HeadlessCms = {
   queryConnection?: Maybe<QueryContentConnection>;
   queryDsl?: Maybe<Array<Maybe<Content>>>;
   queryDslConnection?: Maybe<QueryDslContentConnection>;
-  siteConfiguration: SiteConfiguration;
   sitemap?: Maybe<Sitemap>;
 };
 
@@ -1421,12 +1420,6 @@ export type RichText = {
   macrosAsJson?: Maybe<Scalars['JSON']['output']>;
   processedHtml?: Maybe<Scalars['String']['output']>;
   raw?: Maybe<Scalars['String']['output']>;
-};
-
-/** Configuration for other parts of the site */
-export type SiteConfiguration = {
-  __typename?: 'SiteConfiguration';
-  searchPageHref: Scalars['String']['output'];
 };
 
 /** Site configurator. */
