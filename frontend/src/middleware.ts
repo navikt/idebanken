3a338d00c958bb189cc002231686e48de2039b8f
Amund Lindstøl <alindstol@gmail.com>
2025-11-10 11:30:49 +0100

    allow apsis pro connect-src

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -55,7 +55,7 @@ function getCspHeaderAndAppendToRequestHeaders(req: NextRequest) {
     const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isLocalhost ? "'unsafe-eval'" : ''};
-    connect-src 'self' umami.nav.no survey.skyra.no ingest.staging.skyra.no;
+    connect-src 'self' umami.nav.no survey.skyra.no ingest.staging.skyra.no se.api.anpdm.com;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: ${enonicDomain ?? ''};
     font-src 'self' cdn.nav.no;
