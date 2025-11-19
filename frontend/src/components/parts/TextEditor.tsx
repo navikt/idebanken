94f30cde091deb1220ebbcaf1223e5a59e3ae82c
Igor Koudrik <igor.koudrik@eyeye.no>
2025-11-19 13:00:57 +0100

    Bleed background for TextEditor box

--- frontend/src/components/parts/TextEditor.tsx
+++ frontend/src/components/parts/TextEditor.tsx
@@ -2,22 +2,34 @@ import { htmlRichTextReplacer } from 'utils/richText/html-rich-text-replacer'
 import RichTextView from '@enonic/nextjs-adapter/views/RichTextView'
 import { Part_Idebanken_Text_Editor } from '~/types/generated'
 import { PartData } from '~/types/graphql-types'
+import BleedingBackgroundPageBlock from '../layouts/BleedingBackgroundPageBlock'
 
 export const TextEditorView = (props: PartData<Part_Idebanken_Text_Editor>) => {
-    const { part } = props
+    const { part, meta } = props
+
     const richTextData = part.config?.simpleTextEditor ?? { processedHtml: '[Tomt innhold]' }
-    const box = part.config?.boxColor ? `${part.config.boxColor} rounded-3xl p-6` : ''
+    const bleed = Boolean(part.config?.boxColor)
+    const box = bleed ? `${part.config.boxColor} rounded-xl py-11` : ''
     const halfWidth = part.config?.halfWidth ? 'md:w-1/2' : ''
 
-    return (
+    const content = (
         <div className={`w-full ${halfWidth}`}>
             <RichTextView
                 // @ts-expect-error data.processedHtml is not required
                 data={richTextData ?? {}}
-                meta={props.meta}
-                className={`${box}`}
+                meta={meta}
                 customReplacer={htmlRichTextReplacer}
             />
         </div>
+    )
+
+    if (!bleed) {
+        return content
+    }
+
+    return (
+        <BleedingBackgroundPageBlock marginInline="11" bleedClassName={box}>
+            {content}
+        </BleedingBackgroundPageBlock>
     )
 }
