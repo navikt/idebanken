65936399e567313fda9c003eccb4f6beb384c996
Amund Lindstøl <alindstol@gmail.com>
2025-12-03 12:42:11 +0100

    moved part config queries

--- frontend/src/components/_mappings.ts
+++ frontend/src/components/_mappings.ts
@@ -1,6 +1,7 @@
 import { APP_NAME, ComponentRegistry, richTextQuery } from '@enonic/nextjs-adapter'
 import { commonQuery, commonVariables } from './queries/common'
 import {
+    articleCardListQuery,
     buttonQuery,
     downloadsQuery,
     highlightedBoxMacroQuery,
@@ -9,6 +10,7 @@ import {
     linkCardQuery,
     newsletterQuery,
     tableOfContentsQuery,
+    themeCardListQuery,
     titleIngressQuery,
     videoPartOrMacroQuery,
 } from './queries/parts'
@@ -185,35 +187,12 @@ ComponentRegistry.addPart(`${APP_NAME}:link-card-list`, {
 
 ComponentRegistry.addPart(`${APP_NAME}:article-card-list`, {
     view: ArticlesLinkCardList,
-    configQuery: `{
-        total
-        availableTypeTags { id name }
-        list(offset: 0, count: 5) {
-            url
-            title
-            description
-            image { url caption }
-            themeTags { id name }
-            typeTags { id name }
-        }
-    }`,
+    configQuery: articleCardListQuery,
 })
 
 ComponentRegistry.addPart(`${APP_NAME}:theme-card-list`, {
     view: ThemeCardList,
-    configQuery: `{
-        data(offset: 0, count: 5, path: $path) {
-            total
-            list {
-                url
-                title
-                description
-                image { url caption }
-                themeTags { id name }
-                typeTags { id name }
-            }
-        }
-    }`,
+    configQuery: themeCardListQuery,
 })
 
 ComponentRegistry.addPart(`${APP_NAME}:expansion-card`, {
