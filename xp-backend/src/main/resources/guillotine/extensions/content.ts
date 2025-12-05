53fe84ba9f6476bedc0647bde0bcbdb08437e37c
Amund Lindstøl <alindstol@gmail.com>
2025-12-05 07:18:19 +0100

    created guillotine backlink data

--- xp-backend/src/main/resources/guillotine/extensions/content.ts
+++ xp-backend/src/main/resources/guillotine/extensions/content.ts
@@ -5,6 +5,8 @@ import { EmptyRecord } from '../common-guillotine-types'
 import { forceArray } from '/lib/utils/array-utils'
 import { getSiteConfig } from '/lib/utils/site-config'
 import { runInContext } from '/lib/repos/run-in-context'
+import { Content, get } from '/lib/xp/content'
+import { enonicSitePathToHref } from '/lib/utils/string-utils'
 
 export const contentExtensions = ({
     list,
@@ -16,6 +18,23 @@ export const contentExtensions = ({
 }: GraphQL): Extensions => ({
     resolvers: {
         Content: {
+            backlink: (env: DataFetchingEnvironment<EmptyRecord, LocalContextRecord, Content>) => {
+                const parent = env.source?._path?.replace(/\/[^/]+$/, '')
+                if (!parent) return undefined
+                const parentContent = get({ key: parent })
+                if (!parentContent) return undefined
+
+                const parentTitle =
+                    parentContent._path === '/idebanken'
+                        ? 'Forsiden'
+                        : ((parentContent.data?.shortTitle ||
+                              parentContent.data?.title ||
+                              parentContent.displayName) as string)
+                return {
+                    text: parentTitle,
+                    href: enonicSitePathToHref(parentContent._path),
+                }
+            },
             skyraSlugs: (
                 _env: DataFetchingEnvironment<EmptyRecord, LocalContextRecord, EmptyRecord>
             ): Array<string> => {
@@ -25,12 +44,28 @@ export const contentExtensions = ({
             },
         },
     },
-    types: {},
+    types: {
+        SimpleLink: {
+            description: 'A simple link with text and href',
+            fields: {
+                text: {
+                    type: GraphQLString,
+                },
+                href: {
+                    type: nonNull(GraphQLString),
+                },
+            },
+            interfaces: [],
+        },
+    },
     creationCallbacks: {
         Content: (params): void => {
             params.addFields({
                 skyraSlugs: {
                     type: nonNull(list(nonNull(GraphQLString))),
+                },
+                backlink: {
+                    type: reference('SimpleLink'),
                 },
             })
         },
