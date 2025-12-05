ddedc3face11ebd5ca94e5494ffe2e752aa814ea
Amund Lindstøl <alindstol@gmail.com>
2025-12-05 13:33:34 +0100

    changed theme filter to tag filter for search

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -4,6 +4,6 @@ export const PAGE_TEXT_WIDTH = 'text_width'
 
 // Search
 export const SOK_SEARCH_PARAM = 'ord'
-export const SOK_TEMA_PARAM = 'tema'
+export const SOK_TYPE_TAG_PARAM = 'type'
 export const SOK_PAGE_PARAM = 'page'
 export const SOK_SORT_PARAM = 's'
