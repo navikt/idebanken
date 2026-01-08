d9a79f3e8aaaddda702185ac193643b696dd763a
Amund Lindstøl <alindstol@gmail.com>
2026-01-08 12:22:00 +0100

    downgrade opensearchVersion

--- apps/idebanken-search-api/build.gradle.kts
+++ apps/idebanken-search-api/build.gradle.kts
@@ -32,7 +32,7 @@ repositories {
 
 dependencies {
     val logstashVersion = "8.1"
-    val opensearchVersion = "2.0.5"
+    val opensearchVersion = "1.8.2"
     val opensearchTestcontainersVersion = "2.1.3"
     val jsoupVersion = "1.20.1"
     val testcontainersVersion = "1.21.4"
