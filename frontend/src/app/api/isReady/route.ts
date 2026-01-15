666a0405527428a14184b87fb67d7d57edbd6fe8
Amund Lindstøl <alindstol@gmail.com>
2026-01-15 10:40:59 +0100

    test guillotine endpoint on isReady api

--- frontend/src/app/api/isReady/route.ts
+++ frontend/src/app/api/isReady/route.ts
@@ -1,4 +1,37 @@
+import { fetchGuillotine } from '@enonic/nextjs-adapter/server'
+import { getContentApiUrl } from '@enonic/nextjs-adapter'
+
 export async function GET() {
+    const res = await fetchGuillotine(
+        getContentApiUrl({ contentPath: process.env.ENONIC_API ?? '' }),
+        {
+            site: '/idebanken',
+            default: true,
+            locale: 'no',
+            project: 'idebanken',
+        },
+        {
+            method: 'POST',
+            body: {
+                query: `
+                    query {
+                        guillotine {
+                            get(path: "\${site}") {
+                                _id
+                            }
+                        }
+                    }
+                `,
+            },
+        }
+    )
+
+    if (!res.guillotine?.get?._id) {
+        return new Response('Service Unavailable', {
+            status: 503,
+        })
+    }
+
     return new Response('OK', {
         status: 200,
     })
