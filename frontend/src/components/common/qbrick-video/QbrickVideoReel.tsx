7cf3548964b2d861b0622a91fa60cf9ae3497ed7
Amund Lindstøl <alindstol@gmail.com>
2026-03-06 12:20:31 +0100

    WIP: styling and functionality

--- frontend/src/components/common/qbrick-video/QbrickVideoReel.tsx
+++ frontend/src/components/common/qbrick-video/QbrickVideoReel.tsx
@@ -2,7 +2,7 @@
 
 import style from './QbrickVideo.module.css'
 import React, { useEffect, useId, useState } from 'react'
-import { Box, Button, HStack, InlineMessage, Loader, VStack } from '@navikt/ds-react'
+import { BodyShort, Box, Button, HStack, InlineMessage, VStack } from '@navikt/ds-react'
 import Script from 'next/script'
 import classNames from 'classnames'
 import { useQbrickPlayerState } from '~/components/common/qbrick-video/useQbrickPlayerState'
@@ -17,8 +17,16 @@ import { RENDER_MODE } from '@enonic/nextjs-adapter'
 import Image from 'next/image'
 import { formatImageUrl } from '~/utils/image'
 import { getTimestampFromDuration } from '~/components/common/qbrick-video/videoHelpers'
-import { Modal, ModalBody, ModalFooter } from '@navikt/ds-react/Modal'
 import { ButtonView } from '~/components/parts/Button'
+import { CaretRightIcon, ChevronLeftIcon, ChevronRightIcon } from '@navikt/aksel-icons'
+import { HeadingView } from '~/components/parts/Heading'
+import {
+    Dialog,
+    DialogBody,
+    DialogFooter,
+    DialogHeader,
+    DialogPopup,
+} from '@navikt/ds-react/Dialog'
 
 export const QbrickVideoReel = ({ part, meta }: PartData<Part_Idebanken_Video_Reel>) => {
     const [videoAnalytics, setVideoAnalytics] = useState(false)
@@ -29,6 +37,8 @@ export const QbrickVideoReel = ({ part, meta }: PartData<Part_Idebanken_Video_Re
     if (!videosConfig?.length) {
         return <MissingComponent type={'video-reel'} descriptor={'idebanken:video-reel'} />
     }
+
+    const videoIds = videosConfig.map((_) => useId())
 
     const videos = videosConfig
         .map((video) => {
@@ -87,112 +97,168 @@ export const QbrickVideoReel = ({ part, meta }: PartData<Part_Idebanken_Video_Re
                         videos[currentVideoIndex].setPlayerState('error')
                     }}
                 />
-                {videos.map(({ title, duration, playerState, createAndStartPlayer, poster }, i) => (
-                    <Button
-                        key={title + i}
-                        aria-label={`Se video: ${title}${duration > 0 ? `. Varighet er ${getTimestampFromDuration(duration)} min` : ''}`}
-                        className={classNames(
-                            'aspect-9/16 relative h-[450px] group',
-                            playerState === 'ready' && style.hidden
-                        )}
-                        onClick={() => {
-                            setCurrentVideoIndex(i)
-                            setModalOpen(true)
-                            if (meta.renderMode === RENDER_MODE.NEXT) {
-                                createAndStartPlayer(videoAnalytics)
-                            }
-                        }}>
-                        {poster && (
+                {videos.map(({ title, duration, playerState, createAndStartPlayer, poster }, i) => {
+                    const minutes = Math.floor(duration / 60).toString()
+                    const seconds = Math.round(duration % 60).toString()
+                    return (
+                        <Button
+                            key={title + i}
+                            aria-label={`Se video: ${title}${duration > 0 ? `. Varighet er ${getTimestampFromDuration(duration)} min` : ''}`}
+                            className={classNames(
+                                'aspect-9/16 relative h-[450px] group',
+                                playerState === 'ready' && style.hidden
+                            )}
+                            onClick={() => {
+                                setCurrentVideoIndex(i)
+                                setModalOpen(true)
+                                if (meta.renderMode === RENDER_MODE.NEXT) {
+                                    createAndStartPlayer(videoAnalytics)
+                                }
+                            }}>
                             <Image
                                 unoptimized={meta.renderMode !== RENDER_MODE.NEXT}
                                 className={style.previewImage}
-                                src={formatImageUrl(meta, poster, 1280, 720)}
+                                src={
+                                    formatImageUrl(meta, poster, 720, 1280) ||
+                                    '/images/link-card-fallback.svg'
+                                }
                                 alt=""
                                 fill
                             />
-                        )}
-                        <div
-                            className={classNames(
-                                style.playBadge,
-                                'group-hover:bg-(--ax-bg-strong-hover)!'
-                            )}>
-                            {playerState === 'loading' ? (
-                                <Loader className={style.playLoader} />
-                            ) : (
-                                <svg
-                                    className={style.playArrow}
-                                    focusable="false"
-                                    aria-hidden="true"
-                                    role="img"
-                                    viewBox={'0 0 22 26'}>
-                                    <path fill={'var(--ib-brand-white)'} d={'M22 13 0 26V0Z'} />
-                                </svg>
-                            )}
-                        </div>
-                    </Button>
-                ))}
+                            <VStack
+                                gap={'space-12'}
+                                className={classNames(
+                                    'absolute w-full h-2/5 bottom-0 left-0 rounded-b-(--border-radius-medium)',
+                                    'bg-(--ax-bg-overlay)',
+                                    'px-(--ax-space-32) py-(--ax-space-16)',
+                                    'text-left'
+                                )}>
+                                <HStack gap={'space-8'} className={'flex items-center'}>
+                                    <Box
+                                        className={
+                                            'bg-(--ax-bg-accent-strong-pressed) group-hover:bg-(--ax-bg-accent-strong) w-(--ax-space-28) h-(--ax-space-28) rounded-full flex items-center justify-center'
+                                        }>
+                                        <CaretRightIcon width={24} height={24} />
+                                    </Box>
+                                    <BodyShort className={'underline'}>
+                                        <time dateTime={`PT${minutes}M${seconds}S`}>
+                                            {minutes.padStart(2, '0')}:{seconds.padStart(2, '0')}
+                                        </time>
+                                    </BodyShort>
+                                </HStack>
+                                <HeadingView
+                                    level={'2'}
+                                    size={'xsmall'}
+                                    className={'group-hover:underline'}>
+                                    {title}
+                                </HeadingView>
+                            </VStack>
+                        </Button>
+                    )
+                })}
             </HStack>
-            <Modal
-                open={modalOpen}
-                onClose={() => setModalOpen(false)}
-                className={classNames('aspect-9-16', '', 'w-[90vw] lg:w-[500px]')}
-                header={{
-                    heading: '',
-                    size: 'small',
-                }}>
-                <ModalBody className={''}>
-                    <VStack gap={'space-8'} className={''}>
-                        {videos.map(({ videoContainerId, playerState, title, poster }, i) => (
-                            <Box className={classNames(i !== currentVideoIndex && 'hidden', '')}>
-                                {process.env.NEXT_PUBLIC_ENV !== 'local' ? (
-                                    <>
-                                        {playerState === 'error' && (
-                                            <InlineMessage status={'error'}>
-                                                Det oppsto en feil under lasting av video
-                                            </InlineMessage>
-                                        )}
-                                        <Box
-                                            className={classNames(
-                                                style.macroVideo,
-                                                playerState !== 'ready' && style.hidden,
-                                                '**:rounded-(--border-radius-medium)!',
-                                                'aspect-9/16!'
+            <Dialog open={modalOpen} onOpenChange={setModalOpen}>
+                <DialogPopup
+                    width={'small'}
+                    position={'fullscreen'}
+                    className={'bg-(--ax-bg-overlay) text-(--ax-text-contrast)'}
+                    data-color={'ib-brand-gray'}
+                    onClick={(e) => {
+                        console.log('e.target', e.target)
+                        setModalOpen(false)
+                    }}>
+                    <DialogHeader
+                        className={classNames(
+                            'flex justify-between gap-4 flex-row-reverse max-w-100 mx-auto w-full min-h-25',
+                            '[&>button]:bg-(--ax-bg-strong) [&>button]:text-(--ax-text-contrast)'
+                        )}>
+                        <HeadingView level={'1'} size={'medium'} className={'text-center'}>
+                            {videos[currentVideoIndex].title}
+                        </HeadingView>
+                    </DialogHeader>
+                    <DialogBody className={'bg-none'}>
+                        <VStack gap={'space-8'}>
+                            {videos.map(({ videoContainerId, playerState, title, poster }, i) => (
+                                <Box
+                                    className={classNames(
+                                        i !== currentVideoIndex && 'hidden',
+                                        'flex justify-center'
+                                    )}
+                                    key={videoContainerId}>
+                                    {process.env.NEXT_PUBLIC_ENV !== 'local' ? (
+                                        <>
+                                            {/* Video cannot be rendered on localhost */}
+                                            {playerState === 'error' && (
+                                                <InlineMessage status={'error'}>
+                                                    Det oppsto en feil under lasting av video
+                                                </InlineMessage>
                                             )}
-                                            id={videoContainerId}
-                                            title={title}
-                                            data-qplayer-analytics={videoAnalytics ? 'on' : 'off'}
+                                            <Box
+                                                className={classNames(
+                                                    style.macroVideo,
+                                                    playerState !== 'ready' && style.hidden,
+                                                    '**:rounded-(--border-radius-medium)!',
+                                                    'aspect-9/16! max-h-[70vh]'
+                                                )}
+                                                id={videoContainerId}
+                                                title={title}
+                                                data-qplayer-analytics={
+                                                    videoAnalytics ? 'on' : 'off'
+                                                }
+                                            />
+                                        </>
+                                    ) : (
+                                        <img
+                                            className={
+                                                'object-cover aspect-9/16 max-h-[70vh] rounded-(--border-radius-medium)'
+                                            }
+                                            src={formatImageUrl(meta, poster, 720, 1280)}
+                                            alt=""
                                         />
-                                    </>
-                                ) : (
-                                    <img
-                                        className={'object-cover aspect-9/16'}
-                                        src={formatImageUrl(meta, poster, 1280, 720)}
-                                        alt=""
-                                    />
-                                )}
-                            </Box>
-                        ))}
-                    </VStack>
-                </ModalBody>
-                {videos.length > 1 && (
-                    <ModalFooter>
-                        <HStack className={'flex justify-between flex-nowrap w-full'}>
-                            <ButtonView
-                                meta={meta}
-                                disabled={currentVideoIndex === 0}
-                                config={{ variant: 'tertiary', size: 'medium' }}>
-                                Forrige
-                            </ButtonView>
-                            <ButtonView
-                                meta={meta}
-                                disabled={currentVideoIndex === videos.length - 1}
-                                config={{ variant: 'tertiary', size: 'medium' }}>
-                                Neste
-                            </ButtonView>
-                        </HStack>
-                    </ModalFooter>
-                )}
-            </Modal>
+                                    )}
+                                </Box>
+                            ))}
+                        </VStack>
+                    </DialogBody>
+                    {videos.length > 1 && (
+                        <DialogFooter className={'max-w-100 mx-auto w-full)'}>
+                            <HStack className={'flex justify-between w-full'} gap={'space-32'}>
+                                <ButtonView
+                                    meta={meta}
+                                    icon={<ChevronLeftIcon />}
+                                    disabled={currentVideoIndex === 0}
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        const newIndex = currentVideoIndex - 1
+                                        setCurrentVideoIndex(newIndex)
+                                        if (meta.renderMode === RENDER_MODE.NEXT) {
+                                            videos[newIndex].createAndStartPlayer(videoAnalytics)
+                                        }
+                                    }}
+                                    config={{ variant: 'primary', size: 'medium' }}>
+                                    Forrige
+                                </ButtonView>
+                                <ButtonView
+                                    meta={meta}
+                                    icon={<ChevronRightIcon />}
+                                    iconPosition={'right'}
+                                    disabled={currentVideoIndex === videos.length - 1}
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        const newIndex = currentVideoIndex + 1
+                                        setCurrentVideoIndex(newIndex)
+                                        if (meta.renderMode === RENDER_MODE.NEXT) {
+                                            videos[newIndex].createAndStartPlayer(videoAnalytics)
+                                        }
+                                    }}
+                                    config={{ variant: 'primary', size: 'medium' }}>
+                                    Neste
+                                </ButtonView>
+                            </HStack>
+                        </DialogFooter>
+                    )}
+                </DialogPopup>
+            </Dialog>
         </>
     )
 }
