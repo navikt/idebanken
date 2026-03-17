447d67185a9639aae6b0b8073a565395216b587f
Amund Lindstøl <alindstol@gmail.com>
2026-03-17 10:26:00 +0100

    other versions

--- apps/idebanken-search-api/build.gradle.kts
+++ apps/idebanken-search-api/build.gradle.kts
@@ -33,12 +33,12 @@ repositories {
 dependencies {
     val logstashVersion = "9.0"
     val opensearchVersion = "2.0.3"
-    val opensearchTestcontainersVersion = "4.0.1"
+    val opensearchTestcontainersVersion = "4.1.0"
     val jsoupVersion = "1.20.1"
     val testcontainersVersion = "1.21.4"
     val navnoSearchCommonVersion = "20250820200056-5ed9808"
-    val kotestVersion = "6.0.7"
-    val mockkVersion = "1.14.7"
+    val kotestVersion = "6.1.2"
+    val mockkVersion = "1.14.9"
     val wiremockVersion = "4.3.0"
     val jacksonVersion = "2.21.1"
 
